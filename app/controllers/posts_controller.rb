46bec69575b6797634870e9f5a1d0b1293e2534c
stephenkati <stephenkatuli88@gmail.com>
2023-06-22 09:56:06 +0300

    setup delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+  
   def index
     @posts = Post.includes(:author).where(author_id: params[:user_id])
     @user = User.includes(:posts).find(params[:user_id])
@@ -6,6 +8,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.includes(:author).find_by(author_id: params[:user_id], id: params[:id])
+    @user = @post.author
   end
 
   def new
@@ -16,12 +19,30 @@ class PostsController < ApplicationController
   def create
     @user = current_user
     @post = Post.new(post_params)
+    @post.author_id = @user.id
     if @post.save
-      redirect_to user_posts_path(@user), notice: 'Post created successfully!'
+      redirect_to user_posts_path(@user), flash: { notice: 'Post created successfully!' }
     else
-      redirect_to user_posts_path(@user), alert: 'Failed to create the post.'
+      puts @user
+      puts @post.errors.full_messages
+      flash.now[:alert] = 'Failed to create the post.'
+      render :new
     end
-  end 
+  end
+
+  def destroy
+    @post = Post.includes(:author).find_by(author_id: params[:user_id], id: params[:id])
+    authorize! :destroy, @post
+
+    if @post.destroy
+      flash[:notice] = 'Post deleted successfully!'
+      redirect_to user_posts_path(@post.author)
+    else
+      flash[:alert] = 'Failed to delete the post.'
+      redirect_to user_post_path(@post.author, @post)
+    end
+
+  end
 
   private
   def post_params
